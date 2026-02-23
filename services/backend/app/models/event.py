262e1580ced3b50127cb154a8cd4cc79a3956bf2
Ken Copas <kenny@copas.net>
2026-02-23 08:18:03 -0500

    Update SQLAlchemy models; Generate alembic revision

--- services/backend/app/models/event.py
+++ services/backend/app/models/event.py
@@ -1,16 +1,61 @@
+from enum import Enum
+from typing import Any
 from datetime import datetime
 
-from sqlalchemy import String, DateTime, func
+from sqlalchemy import Enum as SAEnum
+from sqlalchemy import String, DateTime, func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import JSONB
 
 from app.db.base import Base
+
+
+class DeploymentStatus(str, Enum):
+    STARTED = "started"
+    SUCCESS = "success"
+    FAILED = "failed"
 
 
 class Event(Base):
     __tablename__ = "events"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    type: Mapped[str] = mapped_column(String(128), nullable=False)
+    event_type: Mapped[str] = mapped_column(String(128), nullable=False)
+    service_id: Mapped[int] = mapped_column(ForeignKey("services.id"), nullable=False)
+    deployment_id: Mapped[int] = mapped_column(
+        ForeignKey("deployments.id"), nullable=False
+    )
+    occured_at: Mapped["datetime"] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    event_data: Mapped[dict[str, Any] | None] = mapped_column(JSONB, nullable=True)
+
+
+class Service(Base):
+    __tablename__ = "services"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), nullable=False)
+    description: Mapped[str] = mapped_column(String(256), nullable=True)
     created_at: Mapped["datetime"] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    retired_at: Mapped["datetime"] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
+
+
+class Deployment(Base):
+    __tablename__ = "deployments"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image_tag: Mapped[str] = mapped_column(String(7), nullable=False)
+    status: Mapped[DeploymentStatus] = mapped_column(
+        SAEnum(DeploymentStatus, name="deployment_status_enum"), nullable=False
+    )
+    started_at: Mapped["datetime"] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    finished_at: Mapped["datetime"] = mapped_column(
+        DateTime(timezone=True), nullable=True
     )
