d28cda49d6213c207b80bbcdc620fd6412e20eab
Ken Copas <kenny@copas.net>
2026-02-24 12:58:46 -0500

    Add support exclusively for adding deployment.started events

--- services/backend/app/services/event_service.py
+++ services/backend/app/services/event_service.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 
 from app.schemas.base_event import BaseEvent
+from app.schemas.deployment_events import DeploymentStarted
 from app.models.events import IngestedEvent
 
 
@@ -19,22 +20,31 @@ class EventService:
 
     def handle_event(self, event: BaseEvent) -> None:
         # Convert schema into ORM model
+
+        if not isinstance(event, DeploymentStarted):
+            print(
+                "For testing purposes, no-op is chosen for any events of types other than deployment.started. Exiting..."
+            )
+            return
+
         event_model = IngestedEvent(
             event_type=event.event_type,
-            service_id=getattr(event, "service_id", None),
-            deployment_id=getattr(event, "deployment_id", None),
-            occurred_at=event.occurred_at,
-            event_data=event.model_dump(),
+            received_at=datetime.now(),
+            payload=event.model_dump(),
         )
 
         self.db.add(event_model)
         self.db.commit()
-        self.db.refresh(event)
+        self.db.refresh(event_model)
+
+        print(f"Ingested Event ID: {event_model.id}")
 
     def search_events(self) -> List[BaseEvent]:
         """Returns all events in the events table"""
-        return (
+        query_result = (
             self.db.query(IngestedEvent)
-            .order_by(IngestedEvent.occurred_at.desc())
+            .order_by(IngestedEvent.received_at.desc())
             .all()
         )
+        print(query_result[0].__dict__)
+        return query_result
